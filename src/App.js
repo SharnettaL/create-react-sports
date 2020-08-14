249d66e3c2a317a875830460129b04b54cc9028d
Sharnetta <sharnettalamar@yahoo.com>
2020-08-13 21:57:41 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
+import Game from "./components/game/Game";
+import Team from "./components/team/Team";
+import ScoreBoard from "./components/scoreboard/ScoreBoard";
+import raccoon from "./components/assets/raccoon.png";
+import squirrel from "./components/assets/squirrel.png";
+import bunny from "./components/assets/bunny.png";
+import hound from "./components/assets/hound.png";
+import shotSound from "./components/assets/Back+Board.mp3";
+import scoreSound from "./components/assets/Swish+2.mp3";
 
 function App() {
   return (
     <div className="App">
+      <Game />
+      <Team />
+      <ScoreBoard />
+      <img src={raccoon} alt="this is raccoon image" />
+      <img src={squirrel} alt="this is squirrel image" />
+      <img src={bunny} alt="this is bunny image" />
+      <img src={hound} alt="this is hound image" />
+      <audio src={shotSound} />
+      <audio src={scoreSound} />
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
